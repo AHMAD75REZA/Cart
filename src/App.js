3be3b5239752f141cad34ecc0196cf71e91cd34e
ahmad75reza <ahmad75reza>
2023-05-05 12:51:34 +0530

    Increasing quatity and events in react

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import CartItem from './CartItem';
 
 
 function App() {
+
+function showAlert(){
+  alert("Hello!");
+}
+
+function handleInputchange(e){
+  console.log(e.target.value);
+}
   return (
     <div className="App">
-      <CartItem />
-     
+      {/* <CartItem /> */}
+     <button onClick={showAlert}> show Alert</button>
+
+     <input onChange={handleInputchange}/>
     </div>
   );
 }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile pjone',
+      qty:1,
+      img: ''
+    }
+  }
+
   render () {
+    const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -13,6 +24,9 @@ class CartItem extends React.Component {
           <div style={ { color: '#777' } }>Qty: 1</div>
           <div className="cart-item-actions">
             {/* Buttons */}
+            <img alt="increase" className="action-icons" src="https://img.icons8.com/?size=512&id=1501&format=png"/>
+            <img alt="decrease" className="action-icons" src="https://img.icons8.com/?size=512&id=1504&format=png" />
+            <img alt="delete" className="action-icons" src="https://img.icons8.com/?size=512&id=1942&format=png" />
           </div>
         </div>
       </div>
