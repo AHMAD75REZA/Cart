4c93d30302157314fde8594b830894fae59fe361
ahmad75reza <ahmad75reza>
2023-05-22 22:38:43 +0530

    Increase quantity with cartItem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 
-// import './App.css';
-import React from 'react';
-import CartItem from './CartItem';
+// // import './App.css';
+// import React from 'react';
+// import CartItem from './CartItem';
 
+
+// function App() {
+
+// function showAlert(){
+//   alert("Hello!");
+// }
+
+// function handleInputchange(e){
+//   console.log(e.target.value);
+// }
+//   return (
+//     <div className="App">
+//       <CartItem />
+//      <button onClick={showAlert}> show Alert</button>
+
+//      <input onChange={handleInputchange}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState } from "react";
+import Cart from "./Cart";
 
 function App() {
+  const [products, setProducts] = useState([
+    {
+      price: 99,
+      title: "Watch",
+      qty: 1
+    }
+  ]);
 
-function showAlert(){
-  alert("Hello!");
-}
+  const handleIncreaseQuantity = (product) => {
+    console.log("Heyy please inc the qty of ", product);
+    const index = products.indexOf(product);
+    const newProducts = [...products];
+    newProducts[index].qty += 1;
+    setProducts(newProducts);
+  };
 
-function handleInputchange(e){
-  console.log(e.target.value);
-}
+  const handleDecreaseQuantity = (product) => {
+    console.log("Heyy please inc the qty of ", product);
+    const index = products.indexOf(product);
+    if (products[index].qty === 0) {
+      return;
+    }
+    const newProducts = [...products];
+    newProducts[index].qty -= 1;
+    setProducts(newProducts);
+  };
+
+  const handleDeleteProduct = (id) => {
+    const newProducts = products.filter((item) => item.id !== id);
+    setProducts(newProducts);
+  };
+
   return (
     <div className="App">
-      <CartItem />
-     <button onClick={showAlert}> show Alert</button>
-
-     <input onChange={handleInputchange}/>
+      <Cart
+        products={products}
+        onIncreaseQuantity={handleIncreaseQuantity}
+        onDecreaseQuantity={handleDecreaseQuantity}
+        onDeleteProduct={handleDeleteProduct}
+      />
     </div>
   );
 }
 
 export default App;
+
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,60 +1,130 @@
+// import React from 'react';
+
+// class CartItem extends React.Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       price: 999,
+//       title: 'Mobile pjone',
+//       qty:1,
+//       img: ''
+//     }
+//      this.increaseQuantity = this.increaseQuantity.bind(this);
+//   }
+//   increaseQuantity = () => {
+//     // this.state.qty +=1;
+//     // console.log('this', this.state);
+//     // setstate form 1
+//     this.setState({
+//       qty:this.state.qty + 1
+//     });
+    
+//     // setState form2 - if prevState required use this
+//     this.setState((prevState) => {
+//       return{
+//         qty: prevState.qty + 1
+//       }
+//     });
+//   }
+//   render () {
+//     const { price, title, qty } = this.state;
+//     return (
+//       <div className="cart-item">
+//         <div className="left-block">
+//           <img style={styles.image} />
+//         </div>
+//         <div className="right-block">
+//           <div style={ { fontSize: 25 } }>Phone</div>
+//           <div style={ { color: '#777' } }>Rs 999</div>
+//           <div style={ { color: '#777' } }>Qty: 1</div>
+//           <div className="cart-item-actions">
+//             {/* Buttons */}
+//             <img
+//              alt="increase" 
+//              className="action-icons" 
+//              src="https://img.icons8.com/?size=512&id=1501&format=png"
+//              onClick={this.increaseQuantity.bind(this)}
+//              />
+
+//             <img 
+//             alt="decrease" 
+//             className="action-icons" 
+//             src="https://img.icons8.com/?size=512&id=1504&format=png" 
+//             onClick={this.increaseQuantity}
+//             />
+//             <img alt="delete" className="action-icons" src="https://img.icons8.com/?size=512&id=1942&format=png" />
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// const styles = {
+//   image: {
+//     height: 110,
+//     width: 110,
+//     borderRadius: 4,
+//     background: 'blue'
+
+//   }
+// }
+
+// export default CartItem;
+
+
 import React from 'react';
 
-class CartItem extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile pjone',
-      qty:1,
-      img: ''
-    }
-     this.increaseQuantity = this.increaseQuantity.bind(this);
-  }
-  increaseQuantity = () => {
-    console.log('this', this);
-  }
-  render () {
-    const { price, title, qty } = this.state;
-    return (
-      <div className="cart-item">
-        <div className="left-block">
-          <img style={styles.image} />
-        </div>
-        <div className="right-block">
-          <div style={ { fontSize: 25 } }>Phone</div>
-          <div style={ { color: '#777' } }>Rs 999</div>
-          <div style={ { color: '#777' } }>Qty: 1</div>
-          <div className="cart-item-actions">
-            {/* Buttons */}
-            <img
-             alt="increase" 
-             className="action-icons" 
-             src="https://img.icons8.com/?size=512&id=1501&format=png"
-             onClick={this.increaseQuantity.bind(this)}
-             />
-
-            <img 
-            alt="decrease" 
-            className="action-icons" 
+const CartItem = (props) => {
+  const { price, title, qty } = props.product;
+  const {
+    product,
+    onIncreaseQuantity,
+    onDecreaseQuantity,
+    onDeleteProduct
+  } = props;
+  return (
+    <div className="cart-item">
+      
+      <div className="right-block">
+        <div style={ { fontSize: 25 } }>{title}</div>
+        <div style={ { color: '#777' } }>Rs {price} </div>
+        <div style={ { color: '#777' } }>Qty: {qty} </div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
+            alt="increase"
+            className="action-icons"
+            src="https://img.icons8.com/?size=512&id=1501&format=png"
+            //  onClick={this.increaseQuantity.bind(this)}
+            style={styles.img}
+            onClick={() => onIncreaseQuantity(product)}
+          />
+          <img
+            alt="decrease"
+            className="action-icons"
             src="https://img.icons8.com/?size=512&id=1504&format=png" 
-            onClick={this.increaseQuantity}
-            />
-            <img alt="delete" className="action-icons" src="https://img.icons8.com/?size=512&id=1942&format=png" />
-          </div>
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBi32HcSiBA8RBgI6awpRvGRQ--Tqtrj9N4A&usqp=CAU"
+
+            onClick={() => onDeleteProduct(product.id)}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 const styles = {
-  image: {
-    height: 110,
-    width: 110,
+  img: {
+    height: 20,
+    width: 20,
     borderRadius: 4,
-    background: 'blue'
-
+    background: '#ccc'
   }
 }
 
